237fdb8b00190dcdc7ee3ecc69e6f6bbccb460fa
fzjalfred <fzjalfred@gmail.com>
2021-12-07 01:16:56 +0800

    seed 4334 fixed
    Signed-off-by: fzjalfred <fzjalfred@gmail.com>

---
+++ A6/run.sh
@@ -0,0 +1,1 @@
+for i in {1..1000}; do ./soda d $i 1; done 
--- A6/truck.cc
+++ A6/truck.cc
@@ -27,58 +27,62 @@ void Truck::main() {
         yield(rng(1,10));
         try {
             plant.getShipment(sodaList); // need to release when ending.
+            // {
+            //     sodaList[0] = 10;
+            //     sodaList[1] = 10;
+            //     sodaList[2] = 10;
+            //     sodaList[3] = 10;
+            // }
+            uint total_amount = 0;
+            for (int c = 0; c<4; c++){
+                total_amount += sodaList[c];
+            }
+            PRINT(Printer::Kind::Truck,'P', total_amount);
+            //std::cout<<"inventory block ..."<<std::endl;
+
+
+            VendingMachine* machine = mch_list[i];
+            
+            unsigned int *stock = machine->inventory();
+            
+            uint total_remain = 0;
+            for (int c = 0; c<4; c++) {
+                total_remain+= stock[c];
+            } // calculate the remain inventory in this machine.
+            PRINT(Printer::Kind::Truck,'d', i, total_remain);
+            
+            uint total_lack = 0;
+            for (int c = 0; c<4; c++) {
+                stock[c] += sodaList[c];
+                if (stock[c]>maxStockPerFlavour) {
+                    sodaList[c] = stock[c] - maxStockPerFlavour;
+                    stock[c] = maxStockPerFlavour;
+                } else {
+                    total_lack+=maxStockPerFlavour - stock[c];
+                    sodaList[c] = 0;
+                }
+            } // calculate the lack to fullfill this machine.
+            
+            PRINT(Printer::Kind::Truck,'U', i, total_lack);
+            uint cargo_remain = 0;
+            for (int c = 0; c<4; c++) {
+                cargo_remain+= sodaList[c];
+            } // calculate the cargo remain in the truck.
+            PRINT(Printer::Kind::Truck,'D', i, cargo_remain);
+            //std::cout<<"restocked block ..."<<std::endl;
+            machine->restocked(); // restock is completed.
+            
+            if (rng()%100 == 0) { // fix flat tire
+                PRINT(Printer::Kind::Truck,'X');
+                yield(10);
+            }
         } catch (BottlingPlant::Shutdown &) {
             //std::cout<<"Truck is shuting down ..."<<std::endl;
             break;
+        } catch (...) {
+            // inventory() or restock() throw expection.
         }
-        // {
-        //     sodaList[0] = 10;
-        //     sodaList[1] = 10;
-        //     sodaList[2] = 10;
-        //     sodaList[3] = 10;
-        // }
-        uint total_amount = 0;
-        for (int c = 0; c<4; c++){
-            total_amount += sodaList[c];
-        }
-        PRINT(Printer::Kind::Truck,'P', total_amount);
-
-
-
-        VendingMachine* machine = mch_list[i];
         
-        unsigned int *stock = machine->inventory();
-        
-        uint total_remain = 0;
-        for (int c = 0; c<4; c++) {
-            total_remain+= stock[c];
-        } // calculate the remain inventory in this machine.
-        PRINT(Printer::Kind::Truck,'d', i, total_remain);
-        
-        uint total_lack = 0;
-        for (int c = 0; c<4; c++) {
-            stock[c] += sodaList[c];
-            if (stock[c]>maxStockPerFlavour) {
-                sodaList[c] = stock[c] - maxStockPerFlavour;
-                stock[c] = maxStockPerFlavour;
-            } else {
-                total_lack+=maxStockPerFlavour - stock[c];
-                sodaList[c] = 0;
-            }
-        } // calculate the lack to fullfill this machine.
-        
-        PRINT(Printer::Kind::Truck,'U', i, total_lack);
-        uint cargo_remain = 0;
-        for (int c = 0; c<4; c++) {
-            cargo_remain+= sodaList[c];
-        } // calculate the cargo remain in the truck.
-        PRINT(Printer::Kind::Truck,'D', i, cargo_remain);
-        machine->restocked(); // restock is completed.
-        
-        if (rng()%100 == 0) { // fix flat tire
-            PRINT(Printer::Kind::Truck,'X');
-            yield(10);
-        }
         
     }
     PRINT(Printer::Kind::Truck,'F');
